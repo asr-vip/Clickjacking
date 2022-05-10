e5666ecfb5b46ae317c69ea69d8937df74e708f5
Arwa Alromih <asr.vip@gmail.com>
2022-05-10 09:58:07 +0100

    Create follow.js

---
+++ follow.js
@@ -0,0 +1,7 @@
+    <script>
+         function myFunction() {
+  followers = +document.getElementById("followers_no").innerHTML
+  document.getElementById("followers_no").innerHTML = followers+1
+  window.alert("you just followed Jane Doe");
+} 
+    </script>
